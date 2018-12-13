d58785d59789432970098f20ab9d7c3fef6d88bf
Kyle Jeon <kylekjeon@gmail.com>
2018-12-13 13:18:36 -0500

    add logic

--- client/src/store.js
+++ client/src/store.js
@@ -6,11 +6,17 @@ const initialState = {};
 
 const middleware = [thunk];
 
+let devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+if(process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'prod'){
+  devTools = a => a;
+}
+
 const store = createStore(
   rootReducer, 
   initialState, 
   compose(
-    applyMiddleware(...middleware)
+    applyMiddleware(...middleware),
+    devTools
   )
 );
 

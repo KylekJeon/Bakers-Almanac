c7353ded24e7adb06a49e67643c20a167250c4c9
Kyle Jeon <kylekjeon@gmail.com>
2018-12-13 12:56:27 -0500

    remove devtools to test

--- client/src/store.js
+++ client/src/store.js
@@ -6,14 +6,11 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const devTools = process.env.NODE_ENV === 'production' ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION_() : a => a;
-
 const store = createStore(
   rootReducer, 
   initialState, 
   compose(
-    applyMiddleware(...middleware),
-    devTools
+    applyMiddleware(...middleware)
   )
 );
 
